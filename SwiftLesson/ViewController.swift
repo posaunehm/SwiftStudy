a8bb7eca94edc13f0e97906bb9d2139055c1e2bf
Hiroshi Maekawa <posaunehm@gmail.com>
2015-02-16 01:13:10 +0900

    Delegateを用いてFizzBuzz画面を作成

--- SwiftLesson/ViewController.swift
+++ SwiftLesson/ViewController.swift
@@ -8,17 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate{
 
+    @IBOutlet weak var fizzbuzzTextField: UITextField!
+    @IBOutlet weak var fizzBuzzLabel: UILabel!
+    
+    let fizzBuzzFunction = BasicFizzBuzz()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.fizzbuzzTextField.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+     
+        var tempStr = textField.text as NSString
+        tempStr = tempStr.stringByReplacingCharactersInRange(range, withString: string)
+        
+        
+        if let intValue = (tempStr as String).toInt()
+        {
+            fizzBuzzLabel.text = fizzBuzzFunction.convert(intValue)
+        }else{
+            fizzBuzzLabel.text = "???"
+        }
+        
+        return true
+    }
+    
+    
 
 
 }
