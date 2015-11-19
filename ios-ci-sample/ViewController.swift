fe729343a783c93f7bedffb376fe6ab3732d5f11
前川博志 <maekawa@guildworks.jp>
2015-11-20 00:52:57 +0900

    Swiftプロジェクトを初期化（シングルページ）

---
+++ ios-ci-sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-ci-sample
+//
+//  Created by 前川博志 on 2015/11/20.
+//  Copyright © 2015年 前川博志. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
